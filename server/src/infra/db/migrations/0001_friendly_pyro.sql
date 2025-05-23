9119fab3fa5edef5465728ee38eb120c933c950a
Davi <davifssoares2002@gmail.com>
2025-05-22 23:39:26 -0300

    feat: first route created

---
+++ server/src/infra/db/migrations/0001_friendly_pyro.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "links" DROP CONSTRAINT "links_remote_key_unique";--> statement-breakpoint
+ALTER TABLE "links" DROP COLUMN "remote_key";
--- server/src/infra/http/routes/create-link.ts
+++ server/src/infra/http/routes/create-link.ts
@@ -1,5 +1,8 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import z from "zod";
+import { db } from "../../db";
+import { schema } from "../../db/schemas";
+import { eq } from 'drizzle-orm'
 
 export const createLink : FastifyPluginAsyncZod = async(server) => {
     server.post("/link", 
@@ -7,14 +10,38 @@ export const createLink : FastifyPluginAsyncZod = async(server) => {
             schema: {
                 summary: "create link",
                 body: z.object({
-                    original_url: z.string().url()
+                    originalLink: z.string().url(),
+                    shortLink: z.string()
                 }),
                 response: {
-                    201: z.object({ linkId: z.number() }),
-                    400: z.object({ message: z.string() }).describe("link already exists"),
+                    201: z.object({ message: z.string() }),
+                    400: z.object({ message: z.string() })
                 }
             }
         }, async (request, reply) => {
-        return reply.status(201).send({linkId: 1})
-    })
+            const { originalLink, shortLink } = request.body;
+
+            const isValidShortCode = /^[a-zA-Z0-9_-]{4,12}$/.test(shortLink)
+
+            if(!isValidShortCode){
+                return reply.status(400).send({ message: 'Invalid short link format' })
+            }
+
+            const getLinkByShortLink = await db.select()
+                .from(schema.links)
+                .where(eq(schema.links.shortLink, shortLink))
+                .limit(1)
+
+            if(getLinkByShortLink?.length > 0){
+                return reply.status(400).send({ message: 'Short link already added' })
+            }
+
+            await db.insert(schema.links).values({
+                originalLink: originalLink,
+                shortLink: shortLink
+            })
+
+            return reply.status(201).send({ message: "Short link successfully created" })
+        }
+    )
 }
